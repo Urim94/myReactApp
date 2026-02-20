aba8678efec58fa7f3a99c8ef38e151c4e79827e
Tirusew Mishamo <152331298+Urim94@users.noreply.github.com>
2026-02-20 03:35:35 -0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Game from './App'   // Make sure path is correct
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>
+)
